3554e32c56cf78752be67ef9c365c57b731141e3
kis8ya <kis8P@yandex.ru>
2022-03-27 00:35:06 +0300

    initial commit

---
+++ src/main/java/com/github/kis8ya/sburdatajpademo/SburDataJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.kis8ya.sburdatajpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburDataJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburDataJpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kis8ya/sburdatajpademo/SburDataJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kis8ya.sburdatajpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SburDataJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
